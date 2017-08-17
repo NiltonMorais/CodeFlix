71664de541a91dd57ae5e2bdd1dc7d3dfdd56e1a
NiltonMorais <nilton_even@hotmail.com>
2017-08-17 16:36:42 -0300

    Montando formulario do PayPal Plus

--- codeflix-app/src/pages/payment/payment.ts
+++ codeflix-app/src/pages/payment/payment.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from "@angular/core";
+import {IonicPage, NavController, NavParams} from "ionic-angular";
+import scriptjs from "scriptjs";
+
+declare var PAYPAL;
 
 /**
  * Generated class for the PaymentPage page.
@@ -9,16 +12,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  */
 @IonicPage()
 @Component({
-  selector: 'page-payment',
-  templateUrl: 'payment.html',
+    selector: 'page-payment',
+    templateUrl: 'payment.html',
 })
 export class PaymentPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PaymentPage');
-  }
+    ionViewDidLoad() {
+        scriptjs('https://www.paypalobjects.com/webstatic/ppplusdcc/ppplusdcc.min.js', () => {
+            let ppp = PAYPAL.apps.PPP({
+                approvalUrl: '',
+                placeholder: 'ppplus',
+                mode: 'sandbox',
+                country: 'BR',
+                language: 'pt_BR',
+                payerFirstName: 'Nilton',
+                payerLastName: 'Morais',
+                payerEmail: 'admin@user.com',
+                payerTaxId: '34298482456',
+                payerTaxIdType: 'BR_CPF',
+            });
+        });
+    }
 
 }
